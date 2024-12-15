088b12af8b187383154b9f31f0961f9fadb62b52
srijan80 <srijanpoudel800@gmail.com>
2024-12-15 17:15:48 +0545

    completed project

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,8 +1,8 @@
 export const Nav = () => {
   return (
  <>
- <div className="bg-black text-2xl text-white flex items-center justify-between p-4">
- <img src="/src/img/logoimg.png" alt="Logo" className="h-16 object-cover rounded-md" />
+ <div className="bg-black text-2xl text-white flex items-center justify-between p-5">
+ <img src="/src/img/imglogoo.png" alt="Logo" className="h-16 w-16 object-contain rounded-md filter grayscale rotate-45" />
  <div className="text-center flex-grow">
   MUVIGO
  <p className="text-lg mt-2">Find similar movies,music and game based on your search!</p>
