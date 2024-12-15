fa4916598a08b56af23dec82c72679df8e344194
srijan80 <srijanpoudel800@gmail.com>
2024-12-15 17:02:50 +0545

    completed project

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,14 +1,13 @@
 export const Nav = () => {
   return (
-    <>
-    
-      <div className="bg-black text-2xl text-white flex items-center justify-between p-4">
-        <img src="...." alt="Logo" className="h-8" />
-        <div className="text-center flex-grow">
-          MUVIGO
-        <p className="text-lg mt-2">Find similar movies,music and game based on your search!</p>
-        </div>
-      </div>
-    </>
+ <>
+ <div className="bg-black text-2xl text-white flex items-center justify-between p-4">
+ <img src="/src/img/logoimg.png" alt="Logo" className="h-16 object-cover rounded-md" />
+ <div className="text-center flex-grow">
+  MUVIGO
+ <p className="text-lg mt-2">Find similar movies,music and game based on your search!</p>
+ </div>
+ </div>
+ </>
   );
-};
+ };
